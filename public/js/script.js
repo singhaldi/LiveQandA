4d6af9c0a1824c7408b15ae6f8c57992097ca607
Divya <singhaldivya11@gmail.com>
2021-07-23 17:18:55 +0000

    Logs Topic and Question

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,24 @@
 const questionListSelector = document.querySelector("#questionList");
 const inputSelector = document.querySelector("#question");
+const topicSelector = document.querySelector("#topic");
 const submitButton = document.querySelector("#submit");
-//console.log("page loaded");
-let questionList = [
-  
-]
 
+let questionList = [];
 
 const onSubmit = () => {
   console.log(inputSelector.value);
   inputSelector.innerHTML = "";
-}
-
+  topicSelector.innerHTML = "";
+  const payload = {
+    topic: topicSelector.value,
+    question: inputSelector.value
+  };
+  console.log(payload);
+  firebase
+    .database()
+    .ref()
+    .push(payload);
+};
 
 questionList.forEach(question => {
   console.log("function active");
@@ -20,12 +27,13 @@ questionList.forEach(question => {
   questionContainer.classList.add("columns");
   let questionSelector = document.createElement("div");
   questionSelector.classList.add("column");
+  questionSelector.classList.add("is");
   let voteSelector = document.createElement("div");
   voteSelector.classList.add("column");
-  questionContainer.appendChild(questionSelector)
-  questionContainer.appendChild(voteSelector)
+  questionContainer.appendChild(questionSelector);
+  questionContainer.appendChild(voteSelector);
   questionListSelector.appendChild(questionContainer);
   questionSelector.innerHTML = question;
   voteSelector.innerHTML = 0;
-})
+});
 
